a39c2c237b9db711b1dfb52ba88f0b57fdb2b0bd
Dane Petersen <thegreatsunra@gmail.com>
2017-06-05 07:45:12 -0500

    Remove basic-auth

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const auth = require('basic-auth')
 const express = require('express')
 const app = express()
-
-app.use((req, res, next) => {
-  const credentials = auth(req)
-  if (credentials === undefined || credentials.name !== process.env.USERNAME || credentials.pass !== process.env.PASSWORD) {
-    res.statusCode = 401
-    res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"')
-    res.end('Unauthorized')
-  } else {
-    next()
-  }
-})
 
 app.use(express.static('public'))
 
